51b359d826835b3013dcbdcb2e7cc1e5deb2f4d2
Ryan Duan <duanpanda@gmail.com>
2017-02-24 22:11:05 -0500

    reformat code

--- app/src/main/java/com/android/falldetector/Verification.java
+++ app/src/main/java/com/android/falldetector/Verification.java
@@ -156,18 +156,18 @@ public class Verification extends Activity {
             SmsManager man = SmsManager.getDefault();
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
                     != PackageManager.PERMISSION_GRANTED) {
-                 // Should we show an explanation?
-                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.SEND_SMS)) {
-                            // Show an explanation to the user *asynchronously* -- don't block
-                            // this thread waiting for the user's response! After the user
-                            // sees the explanation, try again to request the permission.
-                        } else {
-                            // No explanation needed, we can request the permission.
-                            ActivityCompat.requestPermissions(this,
-                                            new String[]{Manifest.permission.SEND_SMS},
-                                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-                        }
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.SEND_SMS},
+                            MY_PERMISSIONS_REQUEST_SEND_SMS);
+                }
             }
             else {
                 man.sendTextMessage(number, null, msg, null, null);
