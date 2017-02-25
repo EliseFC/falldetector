fb8f6f24183e183aacf34b9d86c873ca153362b5
SS-WQY <qwang4@lakeheadu.ca>
2017-02-25 18:18:23 -0500

    one more name changed

--- app/src/main/java/layout/wave.java
+++ app/src/main/java/layout/wave.java
@@ -13,12 +13,12 @@ import com.android.falldetector.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link wave.OnFragmentInteractionListener} interface
+ * {@link Wave.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link wave#newInstance} factory method to
+ * Use the {@link Wave#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class wave extends Fragment {
+public class Wave extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class wave extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public wave() {
+    public Wave() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class wave extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment wave.
+     * @return A new instance of fragment Wave.
      */
     // TODO: Rename and change types and number of parameters
-    public static wave newInstance(String param1, String param2) {
-        wave fragment = new wave();
+    public static Wave newInstance(String param1, String param2) {
+        Wave fragment = new Wave();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
