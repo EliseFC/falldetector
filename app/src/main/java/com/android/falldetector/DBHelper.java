c7b8474aecfb69949bd403fda03ee3a109ca3245
Ryan Duan <duanpanda@gmail.com>
2017-03-14 22:37:12 -0400

    reformat DBHelper.java

--- app/src/main/java/com/android/falldetector/DBHelper.java
+++ app/src/main/java/com/android/falldetector/DBHelper.java
@@ -13,28 +13,28 @@ import java.util.HashMap;
  * Created by elise on 2017-02-25.
  */
 
-public class DBHelper  extends SQLiteOpenHelper {
-    public static final String TABLE="HISTORY";
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String TABLE = "HISTORY";
 
-    public static final String KEY_ID="time";
-    public static final String KEY_location="location";
-    public static final String KEY_evalu="evaluation";
+    public static final String KEY_ID = "time";
+    public static final String KEY_location = "location";
+    public static final String KEY_evalu = "evaluation";
 
 
-    private static final int DATABASE_VERSION=4;
-    private static final String DATABASE_NAME="DetectInform.db";  //name of the database
+    private static final int DATABASE_VERSION = 4;
+    private static final String DATABASE_NAME = "DetectInform.db";  //name of the database
 
     public DBHelper(Context context) {
-        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         //create data list
-        String CREATE_TABLE_DETECTINFORM="CREATE TABLE " +TABLE
-                + "( " + KEY_ID  + " Time ,"
-                +  " "+KEY_location  + " "+ "Location, "
-                + " "+KEY_evalu  +" integer)";
+        String CREATE_TABLE_DETECTINFORM = "CREATE TABLE " + TABLE
+                + "( " + KEY_ID + " Time ,"
+                + " " + KEY_location + " " + "Location, "
+                + " " + KEY_evalu + " integer)";
         db.execSQL(CREATE_TABLE_DETECTINFORM);
     }
 
@@ -55,7 +55,7 @@ public class DBHelper  extends SQLiteOpenHelper {
         return true;
     }*/
 
-    public boolean insertData (String time_id, String location, int eval) {
+    public boolean insertData(String time_id, String location, int eval) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(KEY_ID, time_id);
@@ -66,35 +66,34 @@ public class DBHelper  extends SQLiteOpenHelper {
     }
 
     //delete information from database by Prime Key
-    public void deleteData(String time_id, String location, int eval){
+    public void deleteData(String time_id, String location, int eval) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE,KEY_ID+"=?",new String[]{String.valueOf(KEY_ID)});
+        db.delete(TABLE, KEY_ID + "=?", new String[]{String.valueOf(KEY_ID)});
         db.close();
     }
 
-    public ArrayList<HashMap<String, String>> getList(){
-        SQLiteDatabase db=this.getReadableDatabase();
-        String selectQuery="SELECT "+
-                KEY_ID+","+
-                KEY_location +","
-                + KEY_evalu+" FROM "+TABLE;
-        ArrayList<HashMap<String,String>> studentList=new ArrayList<HashMap<String, String>>();
-        Cursor cursor=db.rawQuery(selectQuery,null);
+    public ArrayList<HashMap<String, String>> getList() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectQuery = "SELECT " +
+                KEY_ID + "," +
+                KEY_location + ","
+                + KEY_evalu + " FROM " + TABLE;
+        ArrayList<HashMap<String, String>> studentList = new ArrayList<HashMap<String, String>>();
+        Cursor cursor = db.rawQuery(selectQuery, null);
 
-        if(cursor.moveToFirst()){
-            do{
-                HashMap<String,String> table=new HashMap<String,String>();
-                table.put("time",cursor.getString(cursor.getColumnIndex(this.KEY_ID)));
-                table.put("location",cursor.getString(cursor.getColumnIndex(this.KEY_location)));
-                table.put("evaluation",cursor.getString(cursor.getColumnIndex(this.KEY_evalu)));
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> table = new HashMap<String, String>();
+                table.put("time", cursor.getString(cursor.getColumnIndex(this.KEY_ID)));
+                table.put("location", cursor.getString(cursor.getColumnIndex(this.KEY_location)));
+                table.put("evaluation", cursor.getString(cursor.getColumnIndex(this.KEY_evalu)));
                 studentList.add(table);
-            }while(cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         cursor.close();
         db.close();
         return studentList;
     }
-
 
 
 }
